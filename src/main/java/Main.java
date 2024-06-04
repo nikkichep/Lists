d670371e3f757c52ca9cdccabe507ba0ada34324
Nicole <124882220+nikkichep@users.noreply.github.com>
2024-06-04 06:39:52 +0000

    testing lists

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,22 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;// import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    ArrayList<String> fruits = new ArrayList<>();
+
+    fruits.add("apple");
+    fruits.add("banana");
+    fruits.add("orange");
+    fruits.add("grape");
+    fruits.add("melon");
+    
+    int Listsize = fruits.size();
+    String place = fruits.get(3);
+
+    fruits.set(1, "green banana");
+    System.out.println(fruits);
   }
 
   // @Test
